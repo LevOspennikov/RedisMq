a93639a0546761f09f74da0d99fbb2290cc1d31f
Lev Ospennikov <ospennikovlev@gmail.com>
2018-06-29 12:47:47 +0300

    remove unnessary binds

--- src/MessageQueue.js
+++ src/MessageQueue.js
@@ -6,20 +6,21 @@ const NEW_TRY_DELAY = 1000; // in ms;
 const NEW_MESSAGE_GENERATE_DELAY = 500; // in ms;
 const LAST_GENERATOR_TIME = 'Last_time_generator_was_alive';
 const ERRORS_QUEUE_TAG = '-Errors';
+const _DEV_ = false;
 
 class MessageQueue {
     constructor (queueName, isGenerator = false) {
         this._isGenerator = isGenerator;
         this._client = redis.createClient();
         this._client.on('error', function(err) {
-             console.log('Something went wrong ', err)
+             this._log('Error occurred ', err)
         });
         this._queueName = queueName;
-        this._count = 0; 
+        this._count = 0;
     }
 
     run() {
-        if (this._isGenerator) { 
+        if (this._isGenerator) {
             this.generateMessage();
         } else {
             this.checkGeneratorIsAlive();
@@ -30,40 +31,40 @@ class MessageQueue {
         if (this._count == CHECK_GENERATOR_COUNT) {
             this._count -= CHECK_GENERATOR_COUNT;
             return this.checkGeneratorIsAlive();
-        } 
+        }
 
         this._count++;
-        this._client.lpop(this._queueName, function (err, reply) {
-            if (err) throw err;         
+        this._client.lpop(this._queueName, (err, reply) => {
+            if (err) throw err;
             if (reply == null) {
                 setTimeout(this.getMessages.bind(this), NEW_TRY_DELAY);
             } else {
-                if (parseInt(Math.random() * 100) % 20 == 0) { 
-                    console.log('Error occurred in message: ', reply);
+                if (parseInt(Math.random() * 100) % 20 == 0) {
+                    this._log('Error occurred in message: ', reply);
                     this._client.rpush(this._queueName + ERRORS_QUEUE_TAG, reply);
-                } else { 
+                } else {
                     redis.print(err, reply);
                 }
                 setTimeout(this.getMessages.bind(this), NEW_MESSAGE_RECEIVE_DELAY);
             }
-        }.bind(this));
+        });
     }
 
     getErrors() {
-        this._client.lpop(this._queueName + ERRORS_QUEUE_TAG, function (err, reply) {
-            if (err) throw err; 
+        this._client.lpop(this._queueName + ERRORS_QUEUE_TAG, (err, reply) => {
+            if (err) throw err;
             if (reply == null) {
-                console.log('All errors processed');
+                this._log('All errors processed');
                 this._client.quit();
             } else {
-                console.log(reply);
+                this._log(reply);
                 setTimeout(this.getErrors.bind(this), 0);
             }
-        }.bind(this));
+        });
     }
 
     checkGeneratorIsAlive() {
-        console.log('Checking generator is alive...');
+        this._log('Checking generator is alive...');
         this._client.exists(LAST_GENERATOR_TIME, (err, reply) => {
             if (!reply) {
                 this._client.watch(LAST_GENERATOR_TIME);
@@ -72,10 +73,10 @@ class MessageQueue {
                 multiQuery.expire(LAST_GENERATOR_TIME, 10);
                 multiQuery.exec((err, reply) => {
                     if (err) throw err;
-                    if (reply == null) { 
+                    if (reply == null) {
                         this.getMessages();
                     } else {
-                        this._isGenerator = true; 
+                        this._isGenerator = true;
                         this.generateMessage();
                     }
                 })
@@ -85,17 +86,23 @@ class MessageQueue {
         })
     }
 
-    generateMessage() { 
+    generateMessage() {
         const genString = this._queueName +  parseInt(Math.random() * 10) + Date.now();
         const multiQuery = this._client.multi();
         multiQuery.set(LAST_GENERATOR_TIME, 1);
         multiQuery.expire(LAST_GENERATOR_TIME, 10);
         multiQuery.rpush(this._queueName, genString);
         multiQuery.exec((err, reply) => {
-            if (err) throw err; 
+            if (err) throw err;
         });
-        console.log('Message generated: ', genString);
+        this._log('Message generated: ', genString);
         setTimeout(this.generateMessage.bind(this), NEW_MESSAGE_GENERATE_DELAY);
+    }
+
+    _log(...arr) {
+        if (_DEV_) {
+            console.log(...arr);
+        }
     }
 }
 
